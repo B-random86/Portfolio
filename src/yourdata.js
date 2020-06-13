1b02174f9374c6c812ae26e71ce17392af51bdd6
Brandon <eosguitarist27@gmail.com>
2020-06-12 22:21:00 -0700

    first commit portfolio

---
+++ src/yourdata.js
@@ -0,0 +1,82 @@
+export default
+    {
+        //(Please Do Not Remove The comma(,) after every variable)
+        //Change The Website Template
+        //name :'Brandon.',
+        headerTagline: [//Line 1 For Header
+                        "Hi i'm Brandon Curtsinger.",
+                        //Line 2 For Header
+                        'Student of Web Developement. ',
+                        //Line 3 For Header
+    ],
+        //Contact Email
+        contactEmail:'EOSGuitarist@gmail.com',
+        // Add Your About Text Here
+        abouttext: "Im a happy and fun person and enjoy playing video games.",
+        aboutImage:"",
+       //Change This To Hide The Image of About Section (True Or False)
+       ShowAboutImage:false,//true or false (Change Here)
+       // Change Projects Here 
+       projects:[
+           {
+            id: 1,//DO NOT CHANGE THIS (Please)😅
+            title:'React Bootcamp', //Project Title - Add Your Project Title Here
+             service:'Learning React', // Add Your Service Type Here
+            //Project Image - Add Your Project Image Here
+             imageSrc:"https://images.unsplash.com/photo-1558104631-0fa41a8f6c20?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+             //Project URL - Add Your Project Url Here
+             url:'http://.com/'
+            },
+            {
+                id: 2,//DO NOT CHANGE THIS (Please)😅
+                title: 'Bootstrap Bootcamp',
+                service: 'Learning Bootstrap',
+                imageSrc: "https://images.unsplash.com/photo-1554866585-cd94860890b7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+                url: 'http://.com/'
+            },
+            { 
+                id: 3,//DO NOT CHANGE THIS (Please)😅
+                title: 'HTML,CSS,Javascript Bootcamp',
+                service: 'Learning HTML, CSS, Javascript',
+                imageSrc: "https://images.unsplash.com/photo-1511500118080-275313ec90a1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+                url: 'http://.com/'
+            },
+            {
+                id: 4,//DO NOT CHANGE THIS (Please)😅
+                title: 'Project Four',
+                service: 'Branding',
+                imageSrc: "https://images.unsplash.com/photo-1558452919-08ae4aea8e29?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+                url: 'http://.com/'
+           }
+
+                    /*
+
+                    If You Want To Add More Project just Copy and Paste This At The End (Update the id Respectively)
+                ,{
+                id: 5,
+                title: 'Project Five',
+                service: 'Something Amazing',
+                imageSrc: "",
+                url: ''
+            }
+                */
+        ],
+        social: [
+            // Add Or Remove The Link Accordingly
+            {   name:'Github',
+                url:'https://github.com/B-random86'},
+         // {
+         //     name: '',
+         //     url: ''
+         // },
+            {
+                name: 'Twitter',
+                url: 'https://twitter.com/@BCurtsinger_'
+            },
+            {
+                name: 'Instagram',
+                url: 'https://www.instagram.com/brandon.curtsinger'
+            }
+
+        ]
+    }
